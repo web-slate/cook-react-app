d1157f29cefd46f5258222364775112ca598c976
Venkat.R <ramsunvtech@gmail.com>
2021-03-14 00:23:36 +0800

    tech(clean-up): remove material-ui in widgets

--- slim/src/components/widgets/Fields/InputTextField/InputTextField.js
+++ slim/src/components/widgets/Fields/InputTextField/InputTextField.js
@@ -1,40 +1,25 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField'
-
-import useStyles from './InputTextField.style'
 
 export default function InputTextField({
   name,
   initialValue = '',
   value,
-  error,
   placeholder,
-  helperText,
   handleChange,
   onBlur,
 }) {
-  const classes = useStyles()
-
   const onChange = (event) => {
     handleChange(event)
   }
 
   return (
     <div>
-      <TextField
+      <input
         name={name}
-        margin="normal"
-        variant="outlined"
-        error={error}
-        helperText={helperText}
+        type="text"
         placeholder={placeholder}
         value={value || initialValue}
-        className={classes.textFieldContainer}
-        InputProps={{ classes: { input: classes.textField } }}
         onChange={onChange}
-        FormHelperTextProps={{
-          className: classes.helperText,
-        }}
         onBlur={onBlur}
       />
     </div>
--- slim/src/components/widgets/Loader/Spinner/Spinner.js
+++ slim/src/components/widgets/Loader/Spinner/Spinner.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-import useStyles from './Spinner.style'
-
 export default function Spinner(props) {
-  const classes = useStyles()
-
-  return <div className={classes.root}>Spinner goes here...</div>
+  return <div>Spinner goes here...</div>
 }
